257b9176975bc1148d937c74a93b95d4e5acb044
notoraptor <stevenbocco@gmail.com>
2020-01-31 12:59:06 -0500

    Add philox app.

---
+++ app/philox.cpp
@@ -0,0 +1,31 @@
+//
+// Created by notoraptor on 20-01-30.
+//
+
+#include <Random123/philox.h>
+#include <iostream>
+#include <sstream>
+
+#define EXAMPLE_SEED1_U64   R123_64BIT(0xdeadbeef12345678)
+typedef r123::Philox2x64 CBRNG;
+
+int main(int n, char **args) {
+	CBRNG::key_type::value_type seed = EXAMPLE_SEED1_U64; // example of user-settable seed
+
+	if (n == 2) {
+		std::istringstream iss(args[1]);
+		iss >> seed;
+	}
+
+	CBRNG::key_type key = {{seed}};
+	CBRNG::ctr_type ctr = {{0,0}};
+	CBRNG g;
+	std::cout << std::hex << "Philox2x64 with hex key " << key << " (C++)" << std::endl;
+	for(int i=0; i < 10; ++i) {
+		// ctr[0] = i;
+		ctr[1] = i;
+		CBRNG::ctr_type rand = g(ctr, key);
+		std::cout << "ctr: " << ctr << ", rand: " << rand << "\n";
+	}
+	return 0;
+}
